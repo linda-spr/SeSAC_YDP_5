9a28f63a89db4a9d72670291f89eae628e49b1c4
sean-spreatics <sean@spreatics.com>
2023-10-04 16:18:48 +0900

    study: js 배열 합치는 방법

--- 04-javascript/index8.js
+++ 04-javascript/index8.js
@@ -193,3 +193,13 @@ const result = numbers.reduce((accumulator, currentVAlue) => {
   return accumulator + currentVAlue;
 }, initialValue);
 console.log(result); // 115
+
+//////////////////////////
+// 여러 배열 합치는 방법
+// 1. concat()
+const arr_1 = [1, 2, 3];
+const arr_2 = [3, 4, 5];
+console.log(arr_1.concat(arr_2)); // [ 1, 2, 3, 3, 4, 5 ]
+
+// 2. ... spread
+console.log([...arr_1, ...arr_2]); // [ 1, 2, 3, 3, 4, 5 ]
