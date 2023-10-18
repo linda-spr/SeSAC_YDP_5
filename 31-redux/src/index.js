1c3b839150e0dd90f7ba6fb58d86c3e1f071ebfe
sean-spreatics <sean@spreatics.com>
2023-10-18 16:25:40 +0900

    study: redux container 폴더 분리

--- 31-redux/src/index.js
+++ 31-redux/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App3 from './App3';
+import App4 from './App4';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import { composeWithDevTools } from 'redux-devtools-extension';
@@ -14,7 +14,7 @@ const store = configureStore({ reducer: rootReducer }, composeWithDevTools());
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App3 />
+      <App4 />
     </Provider>
   </React.StrictMode>
 );
