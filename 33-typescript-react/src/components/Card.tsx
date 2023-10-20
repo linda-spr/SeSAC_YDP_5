092de7c5fc783b2d3e9279a7a3409d706b160b59
sean-spreatics <sean@spreatics.com>
2023-10-20 18:45:08 +0900

    study: typescript with react

---
+++ 33-typescript-react/src/components/Card.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface CardProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+export default function Card({ title, children }: CardProps) {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <div>{children}</div>
+    </div>
+  );
+}
