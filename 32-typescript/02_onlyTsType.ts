1d7f6084faa431c979a60519e7399ef7907024a1
sean-spreatics <sean@spreatics.com>
2023-10-20 15:49:24 +0900

    study: typescript 문법

--- 32-typescript/02_onlyTsType.ts
+++ 32-typescript/02_onlyTsType.ts
@@ -54,3 +54,109 @@ val = { name: 'sesac' };
 let val2;
 val2 = false;
 val2 = '바이';
+///////////////////////////////////////////////////////////
+// type & interface
+
+// 1. interface
+interface Student {
+  name: string;
+  isPassed: boolean;
+}
+
+const student1: Student = {
+  name: 'jinheyong',
+  isPassed: true,
+  // addr: 'seoul',
+};
+
+const student2: object = {
+  name: 'jinheyong',
+  isPassed: true,
+  addr: 'seoul',
+};
+
+// interface 상속
+// A+, A, B, C, D, F
+// type Score = 'A+' | 'A' | 'B' | 'C' | 'D' | 'F';
+enum Score {
+  Aplus = 'A+',
+  A = 'A',
+  B = 'B',
+  C = 'C',
+  D = 'D',
+  F = 'F',
+}
+
+interface BaseballClub extends Student {
+  position: string;
+  height: number;
+  readonly backNumber?: number; // ?: 있어도 되고 없어도 됨
+  // [grade: number]: string;
+  [grade: number]: Score;
+}
+
+const otani: BaseballClub = {
+  name: 'ontani',
+  isPassed: true,
+  position: 'pitcher',
+  height: 193,
+  backNumber: 17,
+  1: Score.A, // 학년: 점수
+  // 2: 'NP', // error
+};
+console.log(otani);
+
+otani.position = '투수';
+otani['height'] = 200;
+// otani.backNumber = 16; // error : backNumber readonly
+console.log(otani);
+
+// type vs. enum
+type Bp1 = 500 | 700 | 1000;
+enum Bp2 {
+  SM = 500,
+  MD = 700,
+  LG = 1000,
+}
+
+const width1: Bp1 = 500;
+const width2: Bp2 = Bp2.SM;
+
+// 교차 타입: 두개 이상의 타입을 합치는 것
+interface Toy {
+  name: string;
+  start(): void;
+}
+
+interface Car {
+  name: string;
+  color: string;
+  price: number;
+}
+
+type ToyCar = Toy & Car;
+const toyCar: ToyCar = {
+  name: 'tayo',
+  start() {
+    console.log('출발~~');
+  },
+  color: 'blue',
+  price: 5000,
+};
+
+// 2. type
+type Gender = 'F' | 'M';
+type Person = {
+  name: string;
+  age?: number;
+  like?: string[];
+  // gender: string;
+  gender: Gender;
+};
+
+let daniel: Person = {
+  name: 'daniel',
+  gender: 'F', // Gender 타입에 선언된 값만 넣을 수 있음
+  age: 20,
+  like: ['minji', 'hani'],
+};
---
+++ 32-typescript/overriding.js
@@ -0,0 +1,22 @@
+class Car {
+  constructor(year) {
+    this.year = year;
+  }
+
+  getYear() {
+    return this.year;
+  }
+}
+
+class Truck extends Car {
+  // 부모 클래스(Car) getYear 메서드를 재정의 ==> "overriding"
+  getYear() {
+    return this.year + 5;
+  }
+}
+
+const car = new Car(2020);
+const truck = new Truck(2020);
+
+console.log(car.getYear()); // 2020
+console.log(truck.getYear()); // 2025
